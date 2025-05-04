94c13f88c7263ee9d36e91dcbcc25e10c63c99b3
Muhamad Kamil Muhamad Noor <kamilmatnoor@gmail.com>
2025-05-04 15:04:31 +0800

    chore: add html form for new product

--- src/pages/AddProductPage.js
+++ src/pages/AddProductPage.js
@@ -2,13 +2,33 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const AddProductPage = () => {
-  const navigate = useNavigate();
-  return (
-    <div style={{ padding: '20px' }}>
-      <h2>New Product</h2>
-      <button onClick={() => navigate('/')}>Back</button>
-    </div>
-  );
+    const navigate = useNavigate();
+    return (
+        <div style={{ padding: '20px' }}>
+            <h2>New Product</h2>
+            <form>
+                <div>
+                    <label>UPC12 Barcode:</label>
+                    <input name="product_barcode"/>
+                </div>
+                <div>
+                    <label>Product Brand:</label>
+                    <input name="product_brand"/>
+
+                </div>
+                <div>
+                    <label>Product Name:</label>
+                    <input name="product_name"/>
+                </div>
+                <div>
+                    <label>Product Description:</label>
+                    <input name="product_description"/>
+                </div>
+                <button onClick={() => navigate('/')}>Back</button>
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    );
 };
 
 export default AddProductPage;
