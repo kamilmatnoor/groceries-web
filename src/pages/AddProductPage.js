2b45beb48a3f373530aa828454685b2182df97d8
Muhamad Kamil Muhamad Noor <kamilmatnoor@gmail.com>
2025-05-04 15:13:52 +0800

    chore: add input field onchange listener for all inputs to update product object

--- src/pages/AddProductPage.js
+++ src/pages/AddProductPage.js
@@ -3,26 +3,32 @@ import { useNavigate } from 'react-router-dom';
 
 const AddProductPage = () => {
     const navigate = useNavigate();
+    const [product, setProduct] = useState({ product_barcode: '', product_brand: '', product_name: '', product_description: '' });
+
+    const onInputFieldChanged = (e) => {
+        const { name, value } = e.target;
+        setProduct(prev => ({ ...prev, [name]: value }));
+    };
+
     return (
         <div style={{ padding: '20px' }}>
             <h2>New Product</h2>
             <form>
                 <div>
                     <label>UPC12 Barcode:</label>
-                    <input name="product_barcode"/>
+                    <input name="product_barcode" value={product.product_barcode} onChange={onInputFieldChanged} />
                 </div>
                 <div>
                     <label>Product Brand:</label>
-                    <input name="product_brand"/>
-
+                    <input name="product_brand" value={product.product_brand} onChange={onInputFieldChanged} />
                 </div>
                 <div>
                     <label>Product Name:</label>
-                    <input name="product_name"/>
+                    <input name="product_name" value={product.product_name} onChange={onInputFieldChanged} />
                 </div>
                 <div>
                     <label>Product Description:</label>
-                    <input name="product_description"/>
+                    <input name="product_description" value={product.product_description} onChange={onInputFieldChanged} />
                 </div>
                 <button onClick={() => navigate('/')}>Back</button>
                 <button type="submit">Submit</button>
