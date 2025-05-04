fd51538f103cd690cfc9fa17f9ac495dcfce843b
Muhamad Kamil Muhamad Noor <kamilmatnoor@gmail.com>
2025-05-04 15:40:09 +0800

    chore: move mock data into api file

---
+++ src/api/productsApi.js
@@ -0,0 +1,15 @@
+const productsMock = {
+    products: Array.from({ length: 100 }, (_, i) => ({
+        id: String(i + 1),
+        product_barcode: `${100000000000 + i}`,
+        product_brand: `Brand ${String.fromCharCode(65 + (i % 26))}`,
+        product_name: `Name ${String.fromCharCode(65 + (i % 26))}${i + 1}`,
+        product_description: `Description ${String.fromCharCode(65 + (i % 26))}${i + 1}`,
+    })),
+};
+
+export default productsMock;
+
+export async function addProduct(product) {
+    return products[0];
+}
--- src/pages/ProductsPage.js
+++ src/pages/ProductsPage.js
@@ -1,15 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
-const productsMockApi = {
-  products: Array.from({ length: 100 }, (_, i) => ({
-      id: String(i + 1),
-      product_barcode: `${100000000000 + i}`,
-      product_brand: `Brand ${String.fromCharCode(65 + (i % 26))}`,
-      product_name: `Name ${String.fromCharCode(65 + (i % 26))}${i + 1}`,
-      product_description: `Description ${String.fromCharCode(65 + (i % 26))}${i + 1}`,
-  })),
-};
+import productsMock from "../api/productsApi";
 
 const ProductsPage = () => {
   const navigate = useNavigate();
@@ -18,7 +10,7 @@ const ProductsPage = () => {
       <h2>Products</h2>
       <button onClick={() => navigate('/add')}>Add New</button>
       <ul>
-        {productsMockApi.products.map(p => (
+        {productsMock.products.map(p => (
           <li key={p.id} style={{ margin: '10px 0', borderBottom: '1px solid #ccc', paddingBottom: '5px' }}>
             <div>UPC12 Barcode: {p.product_barcode}</div>
             <div>Brand: {p.product_brand}</div>
