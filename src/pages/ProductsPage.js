d9a3de59be00ad139bc31e854f7c47704df51643
Muhamad Kamil Muhamad Noor <kamilmatnoor@gmail.com>
2025-05-05 12:23:12 +0800

    chore: remove a tag

--- src/pages/ProductsPage.js
+++ src/pages/ProductsPage.js
@@ -163,9 +163,7 @@ const ProductsPage = () => {
       <div className="flex flex-wrap gap-4">
         {products.map((product, index) => (
           <div key={index} className="max-w-64 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
-            <a href="#">
-              <img className="rounded-t-lg w-full" src={imageDefault} alt="{product.name}" />
-            </a>
+            <img className="rounded-t-lg w-full" src={imageDefault} alt="{product.name}" />
             <div className="p-5">
               <p className="text-sm font-thin text-gray-700 dark:text-gray-400">Product</p>
               <p className="mb-2 text-sm text-gray-800 dark:text-gray-400">{product.product_name}</p>
