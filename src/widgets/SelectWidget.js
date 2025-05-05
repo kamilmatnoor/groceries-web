d27b75457a181044b1da9f3eda71f02dbbc05801
Muhamad Kamil Muhamad Noor <kamilmatnoor@gmail.com>
2025-05-05 11:35:02 +0800

    feat(widget): add select widget

---
+++ src/widgets/SelectWidget.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export const SelectWidget = ({ value, onChange, options, className = '', ...props }) => {
+  return (
+    <select
+      value={value}
+      onChange={onChange}
+      className={`bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2 dark:bg-gray-700 dark:border-gray-600 dark:text-white ${className}`}
+      {...props}
+    >
+      {options.map((opt) => (
+        <option key={opt.value} value={opt.value}>
+          {opt.label}
+        </option>
+      ))}
+    </select>
+  );
+};
